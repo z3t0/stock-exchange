885b064ac083ab83daccecc9f7679f1ad383577d
Rafi Khan <zetoslab@gmail.com>
2018-05-06 12:57:51 -0400

    Modified Stock
    Added attributes order_history and total_shares
    order_history: will be an array of Orders
    total_shares: will be used to ensure that infinite shares cannot be sold

--- src/stock_exchange/stock.py
+++ src/stock_exchange/stock.py
@@ -1,6 +1,8 @@
-class Stock():
+class Stock:
     """ Defines a Stock Object
     """
-    def __init__(self, symbol, value):
+    def __init__(self, symbol, value, total_shares=100):
         self.symbol = symbol
         self.value = value
+        self.order_history = []
+        self.total_shares = total_shares
--- tests/test_stock.py
+++ tests/test_stock.py
@@ -8,6 +8,8 @@ class TestStockMethods(unittest.TestCase):
     def test_init(self):
         self.assertEqual(stock.symbol, 'ABC')
         self.assertEqual(stock.value, 1)
+        self.assertEqual(stock.order_history, [])
+        self.assertEqual(stock.total_shares, 100)
 
 if __name__ == '__main__':
     unittest.main()
