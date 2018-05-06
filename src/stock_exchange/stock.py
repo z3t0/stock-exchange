2d3e4a36f0b4e41bbab4757b89f7cdf6bb63c47a
Rafi Khan <zetoslab@gmail.com>
2018-05-06 15:58:20 -0400

    Stock keeps track of order history
    Every transaction on a stock should keep track of its history by
    appending the order to stock.order_history

--- src/stock_exchange/stock.py
+++ src/stock_exchange/stock.py
@@ -8,14 +8,16 @@ class Stock:
         self.total_shares = total_shares
         self.sold_shares = 0
 
-    def sell(self, shares):
-        if self.sold_shares - shares < 0:
+    def sell(self, order):
+        if self.sold_shares - order.shares < 0:
             raise Exception("Cannot sell more shares than currently sold")
         else:
-            self.sold_shares -= shares
+            self.sold_shares -= order.shares
+            self.order_history.append(order)
 
-    def buy(self, shares):
-        if self.total_shares - self.sold_shares < shares:
+    def buy(self, order):
+        if self.total_shares - self.sold_shares < order.shares:
             raise Exception("Error: cannot buy more shares than exist")
         else:
-            self.sold_shares += shares
+            self.sold_shares += order.shares
+            self.order_history.append(order)
--- tests/test_stock.py
+++ tests/test_stock.py
@@ -1,10 +1,12 @@
 import unittest
+from unittest.mock import Mock
 from stock_exchange.stock import Stock
 
 class TestStockMethods(unittest.TestCase):
 
     def setUp(self):
         self.stock = Stock('ABC', 1)
+        self.order = Mock()
 
     def test_init(self):
         self.assertEqual(self.stock.symbol, 'ABC')
@@ -14,24 +16,33 @@ class TestStockMethods(unittest.TestCase):
         self.assertEqual(self.stock.sold_shares, 0)
 
     def test_buy(self):
-        self.stock.buy(100)
+        self.order.shares = 100
+
+
+        self.stock.buy(self.order)
 
         self.assertEqual(self.stock.sold_shares, 100)
+        self.assertEqual(self.stock.order_history[-1], self.order)
 
     def test_buy_fail(self):
+        self.order.shares = 100
         self.assertRaises(Exception, self.stock.buy, 200)
-                                    
+        self.assertEqual(len(self.stock.order_history), 0)
 
     def test_sell(self):
+        self.order.shares = 100
+
         # So that shares can be sold
         self.stock.sold_shares = 100 
 
-        self.stock.sell(100)
+        self.stock.sell(self.order)
 
         self.assertEqual(self.stock.sold_shares, 0)
+        self.assertEqual(self.stock.order_history[-1], self.order)
 
     def test_sell_fail(self):
-        self.assertRaises(Exception, self.stock.sell, 100)
+        self.order.shares = 100
+        self.assertRaises(Exception, self.stock.sell, self.order)
 
 if __name__ == '__main__':
     unittest.main()
