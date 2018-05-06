e7e60279953574fd8c78c2fde79dd4cac983d346
Rafi Khan <zetoslab@gmail.com>
2018-05-06 15:21:18 -0400

    Fixed failing test case
    The test case was failing because one test was run before the other
    and it modified a state that was expected to not be changed by the
    latter test

--- tests/test_stock.py
+++ tests/test_stock.py
@@ -1,24 +1,26 @@
 import unittest
 from stock_exchange.stock import Stock
 
-stock = Stock('ABC', 1)
-
 class TestStockMethods(unittest.TestCase):
 
-    def test_init(self):
-        self.assertEqual(stock.symbol, 'ABC')
-        self.assertEqual(stock.value, 1)
-        self.assertEqual(stock.order_history, [])
-        self.assertEqual(stock.total_shares, 100)
+    def setUp(self):
+        self.stock = Stock('ABC', 1)
 
+    def test_init(self):
+        self.assertEqual(self.stock.symbol, 'ABC')
+        self.assertEqual(self.stock.value, 1)
+        self.assertEqual(self.stock.order_history, [])
+        self.assertEqual(self.stock.total_shares, 100)
+
+        print(self.stock.sold_shares)
         # should be 0, but is 100
-        self.assertEqual(stock.sold_shares, 0)
+        self.assertEqual(self.stock.sold_shares, 0)
 
     def test_buy(self):
-        stock.buy(100)
+        self.stock.buy(100)
 
         # sets it to 100
-        self.assertEqual(stock.sold_shares, 100)
+        self.assertEqual(self.stock.sold_shares, 100)
 
     # def test_sell(self):
     #     stock.sell(100)
