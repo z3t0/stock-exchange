f7613ccab64e320a3eba7eb83791bebd8e95e0b1
Rafi Khan <zetoslab@gmail.com>
2018-05-06 14:39:56 -0400

    missed some tests

--- src/stock_exchange/stock.py
+++ src/stock_exchange/stock.py
@@ -6,3 +6,17 @@ class Stock:
         self.value = value
         self.order_history = []
         self.total_shares = total_shares
+        self.sold_shares = 0
+
+    def sell(self, shares):
+        if self.sold_shares - shares < 0:
+            raise Exception("Cannot sell more shares than currently sold")
+        else:
+            self.sold_shares -= shares
+
+    def buy(self, shares):
+        if self.total_shares - self.sold_shares < shares:
+            print("Error: cannot buy more shares than exist")
+        else:
+            self.sold_shares += shares
+        
--- tests/test_stock.py
+++ tests/test_stock.py
@@ -11,5 +11,21 @@ class TestStockMethods(unittest.TestCase):
         self.assertEqual(stock.order_history, [])
         self.assertEqual(stock.total_shares, 100)
 
+        # should be 0, but is 100
+        self.assertEqual(stock.sold_shares, 0)
+
+    def test_buy(self):
+        stock.buy(100)
+
+        # sets it to 100
+        self.assertEqual(stock.sold_shares, 100)
+
+    # def test_sell(self):
+    #     stock.sell(100)
+
+    #     self.assertEqual(stock.sold_shares, 0)
+
+
+
 if __name__ == '__main__':
     unittest.main()
